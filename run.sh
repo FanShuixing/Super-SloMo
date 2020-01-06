1f74090cec916ffcb9fd54f16a8b7c502e91d054
pan <1094170587@qq.com>
2020-01-06 20:30:26 +0800

    fix output dir

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 python data/create_dataset.py --videos_folder /input0/original_high_fps_videos --dataset_folder custom_dataset/
 
 #测试
-#python video_to_slomo.py --ffmpeg ./  --video ../input/output_20s.mp4 --sf 4 --checkpoint ../SuperSloMo.ckpt --fps 25 --output /output/output/out_x4.mkv
+#python video_to_slomo.py --ffmpeg ./  --video ../input/output_20s.mp4 --sf 4 --checkpoint ../SuperSloMo.ckpt --fps 25 --output /output/out_x4.mkv
 #裁剪视频
 #ffmpeg -ss 00:00:00 -t 00:00:20 -i test.mp4 -vcodec copy -acodec copy output_10s.mp4
 
